4699be5d81a0c143f7fab1bb3ce70af590699d7d
Demystify FP <demystifyfp@gmail.com>
2017-08-17 23:33:11 +0530

    simplifies dotliquid templates and rendering

--- src/FsTweet.Web/FsTweet.Web.fs
+++ src/FsTweet.Web/FsTweet.Web.fs
@@ -1,14 +1,24 @@
 module FsTweet.Web
 
 open Suave
+open Suave.Filters
+open Suave.Operators
 open Suave.DotLiquid
+open System.IO
+open System.Reflection
 
+let currentPath =
+  Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+
+let initDotLiquid _ =
+  setCSharpNamingConvention ()
+  let templatesDir = Path.Combine(currentPath, "views")
+  setTemplatesDir templatesDir
 
 [<EntryPoint>]
 let main argv =
-  setCSharpNamingConvention ()
-  setTemplatesDir View.viewsDir
-  
-  let app = SuaveApp.init ()
+  initDotLiquid ()  
+  let app = 
+    path "/" >=> page "guest/home.liquid" ""
   startWebServer defaultConfig app
   0
--- src/FsTweet.Web/View.fs
+++
@@ -1,9 +0,0 @@
-module FsTweet.View
-
-open System.IO
-open System.Reflection
-
-let viewsDir = 
-  let currentPath =
-    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
-  Path.Combine(currentPath, "views")
