aed61a8342bef260d856e4d7582121b6fed8001f
Demystify FP <demystifyfp@gmail.com>
2017-08-20 11:00:11 +0530

    adds validation

--- src/FsTweet.Web/UserSignup.fs
+++ src/FsTweet.Web/UserSignup.fs
@@ -1,11 +1,64 @@
 namespace UserSignup
 
+module Domain =
+  open Chessie.ErrorHandling
+
+  type Username = private Username of string with
+    static member TryCreate (username : string) =
+      match username with
+      | null | ""  -> fail "Username should not be empty"
+      | x when x.Length > 12 -> fail "Username should not be more than 12 characters"
+      | x -> Username x |> ok
+    member this.Value = 
+      let (Username username) = this
+      username
+
+  type EmailAddress = private EmailAddress of string with
+    member this.Value =
+      let (EmailAddress emailAddress) = this
+      emailAddress
+    static member TryCreate (emailAddress : string) =
+     try 
+       new System.Net.Mail.MailAddress(emailAddress) |> ignore
+       EmailAddress emailAddress |> ok
+     with
+       | _ -> fail "Invalid Email Address"
+
+  type Password = private Password of string with 
+    member this.Value =
+      let (Password password) = this
+      password
+    static member TryCreate (password : string) =
+      match password with
+      | null | ""  -> fail "Password should not be empty"
+      | x when x.Length < 4 || x.Length > 8 -> fail "Password should contain only 4-8 characters"
+      | x -> Password x |> ok
+
+  type SignupUser = {
+    Username : Username
+    Password : Password
+    EmailAddress : EmailAddress
+  }
+  with static member TryCreate (username, password, email) =
+        trial {
+          let! username = Username.TryCreate username
+          let! password = Password.TryCreate password
+          let! emailAddress = EmailAddress.TryCreate email
+          return {
+            Username = username
+            Password = password
+            EmailAddress = emailAddress
+          }
+        }
+
 module Suave =
   open Suave
   open Suave.Filters
   open Suave.Operators
   open Suave.DotLiquid
   open Suave.Form
+  open Domain
+  open Chessie.ErrorHandling
 
   type UserSignupViewModel = {
     Username : string
@@ -20,20 +73,29 @@ module Suave =
     Error = None
   }
 
+  let signupTemplatePath = "user/signup.liquid" 
+
   let handleUserSignup ctx = async {
     match bindEmptyForm ctx.request with
-    | Choice1Of2 (userSignupViewModel : UserSignupViewModel) ->
-      printfn "%A" userSignupViewModel
-      return! Redirection.FOUND "/signup" ctx
+    | Choice1Of2 (vm : UserSignupViewModel) ->
+      let result =
+        SignupUser.TryCreate (vm.Username, vm.Password, vm.Email)
+      let onSuccess (signupUser, _) =
+        printfn "%A" signupUser
+        Redirection.FOUND "/signup" ctx
+      let onFailure msgs =
+        let viewModel = {vm with Error = Some (List.head msgs)}
+        page signupTemplatePath viewModel ctx
+      return! either onSuccess onFailure result
     | Choice2Of2 err ->
       let viewModel = {emptyUserSignupViewModel with Error = Some err}
-      return! page "user/signup.liquid" viewModel ctx
+      return! page signupTemplatePath viewModel ctx
   }
 
   let webPart () =
     path "/signup" 
       >=> choose [
-        GET >=> page "user/signup.liquid" emptyUserSignupViewModel
+        GET >=> page signupTemplatePath emptyUserSignupViewModel
         POST >=> handleUserSignup
       ]
       
