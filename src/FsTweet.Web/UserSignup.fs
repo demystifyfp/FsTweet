facea4b03e92cf0814553f79a962e5e2726db993
Demystify FP <demystifyfp@gmail.com>
2017-09-16 20:26:34 +0530

    adds base64URLEncoding

--- src/FsTweet.Web/UserSignup.fs
+++ src/FsTweet.Web/UserSignup.fs
@@ -61,6 +61,12 @@ module Domain =
     static member Create (password : Password) =
       BCrypt.HashPassword(password.Value)
       |> PasswordHash
+  
+  let base64URLEncoding bytes =
+    let base64String = 
+       System.Convert.ToBase64String bytes
+    base64String.TrimEnd([|'='|])
+      .Replace('+', '-').Replace('/', '_')
       
   type VerificationCode = private VerificationCode of string with
     member this.Value =
@@ -72,7 +78,7 @@ module Domain =
       let b : byte [] = 
         Array.zeroCreate verificationCodeLength
       rngCsp.GetBytes(b)
-      System.Convert.ToBase64String b
+      base64URLEncoding b
       |> VerificationCode 
 
   type CreateUserRequest = {
