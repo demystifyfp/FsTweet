8fcf698866b8135ae4510b72c03780c395e9845a
Demystify FP <demystifyfp@gmail.com>
2017-10-07 12:54:24 +0530

    adds post and create post

---
+++ src/FsTweet.Web/Tweet.fs
@@ -0,0 +1,38 @@
+namespace Tweet
+open User
+open Chessie.ErrorHandling
+
+type PostId = PostId of System.Guid
+
+type Post = private Post of string with
+  static member TryCreate (post : string) =
+    match post with
+    | null | ""  -> fail "Tweet should not be empty"
+    | x when x.Length > 140 -> fail "Tweet should not be more than 140 characters"
+    | x -> Post x |> ok
+  member this.Value = 
+    let (Post post) = this
+    post
+
+type CreatePost = UserId -> Post -> AsyncResult<PostId, System.Exception>
+
+
+module Persistence =
+
+  open User
+  open Database
+  open System
+
+  let createPost (getDataCtx : GetDataContext) (UserId userId) (post : Post) = asyncTrial {
+    let ctx = getDataCtx()
+    let newTweet = ctx.Public.Tweets.Create()
+    let newPostId = Guid.NewGuid()
+
+    newTweet.UserId <- userId
+    newTweet.Id <- newPostId
+    newTweet.Post <- post.Value
+    newTweet.TweetedAt <- DateTimeOffset.Now.DateTime
+
+    do! submitUpdates ctx 
+    return PostId newPostId
+  }
