cd5f68e75624307cbd32fdae27c440008ffc2cea
Demystify FP <demystifyfp@gmail.com>
2017-10-24 17:58:54 +0530

    refactors user profile page

--- src/FsTweet.Web/UserProfile.fs
+++ src/FsTweet.Web/UserProfile.fs
@@ -1,13 +1,61 @@
 namespace UserProfile
 
+module Domain =
+  open User
+  open System.Security.Cryptography
+  open Chessie.ErrorHandling
+  open System
+
+  type UserProfile = {
+    User : User
+    GravatarUrl : string
+    IsSelf : bool
+  }
+  let gravatarUrl (emailAddress : UserEmailAddress) =
+    use md5 = MD5.Create()
+    emailAddress.Value.Trim().ToLowerInvariant()  
+    |> System.Text.Encoding.Default.GetBytes
+    |> md5.ComputeHash
+    |> Array.map (fun b -> b.ToString("x2"))
+    |> String.concat ""
+    |> sprintf "http://www.gravatar.com/avatar/%s?s=200"
+
+  let newProfile user = { 
+    User = user
+    GravatarUrl = gravatarUrl user.EmailAddress
+    IsSelf = false
+  }
+
+  let findUserProfile (findUser : FindUser) username = asyncTrial {
+    let! userMayBe = findUser username
+    match userMayBe with
+    | None -> return None
+    | Some user -> 
+       return Some (newProfile user)
+  }
+
+  let handleUserProfile findUserProfile (username : Username) loggedInUser  = asyncTrial {
+    match loggedInUser with
+    | None -> 
+      return! findUserProfile username
+    | Some (user : User) -> 
+      if user.Username = username then
+        let userProfile =
+          {newProfile user with IsSelf = true}
+        return Some userProfile
+      else  
+        return! findUserProfile username
+  }
+
 module Suave =
   open Database
   open Suave.Filters
   open Auth.Suave
   open Suave
+  open Domain
   open User
   open Suave.DotLiquid
-  open System.Security.Cryptography
+  open Chessie.ErrorHandling
   open Chessie
 
   type UserProfileViewModel = {
@@ -21,85 +69,58 @@ module Suave =
     AppId : string
   }
 
-  let emptyUserProfileViewModel = {
-    Username = ""
-    GravatarUrl = ""
-    IsLoggedIn = false
-    IsSelf = false
-    UserId = 0
-    UserFeedToken = ""
-    ApiKey = ""
-    AppId = ""
-  }
+  let newUserProfileViewModel (getStreamClient : GetStream.Client) (userProfile : UserProfile) = 
+    let (UserId userId) = userProfile.User.UserId
+    let userFeed = GetStream.userFeed getStreamClient userId
+    {
+      Username = userProfile.User.Username.Value
+      GravatarUrl = userProfile.GravatarUrl
+      IsLoggedIn = false
+      IsSelf = false
+      UserId = userId
+      UserFeedToken = userFeed.ReadOnlyToken
+      ApiKey = getStreamClient.Config.ApiKey
+      AppId = getStreamClient.Config.AppId
+    }
 
-  let gravatarUrl (emailAddress : UserEmailAddress) =
-    use md5 = MD5.Create()
-    emailAddress.Value.Trim().ToLowerInvariant()  
-    |> System.Text.Encoding.Default.GetBytes
-    |> md5.ComputeHash
-    |> Array.map (fun b -> b.ToString("x2"))
-    |> String.concat ""
-    |> sprintf "http://www.gravatar.com/avatar/%s?s=200"
+  let renderUserProfilePage (vm : UserProfileViewModel) = 
+    page "user/profile.liquid" vm
 
-  let onFindUserSuccess getStreamClient userProfileViewModel (userMayBe : User option) = 
-    match userMayBe with
-    | None -> 
-      let msg = 
-        sprintf "User '%s' not found" userProfileViewModel.Username
-      page "not_found.liquid" msg
-    | Some user -> 
-      let (UserId userId) = user.UserId
-      let userFeed = 
-        GetStream.userFeed getStreamClient userId
-      let vm = 
-        { userProfileViewModel with 
-            GravatarUrl = gravatarUrl user.EmailAddress
-            Username = user.Username.Value
-            UserId = userId
-            UserFeedToken = userFeed.ReadOnlyToken
-        }
-      page "user/profile.liquid" vm
+  let renderProfileNotFound =
+    page "not_found.liquid" "user not found"
+  
+  let handleUserProfileAsyncResult newUserProfileViewModel loggedInUser aResult = async {
+    let! result = aResult |> Async.ofAsyncResult
+    match result with
+    | Success (Some (userProfile : UserProfile)) -> 
+      let vm = {
+        newUserProfileViewModel userProfile with
+          IsLoggedIn = Option.isSome loggedInUser
+          IsSelf = userProfile.IsSelf }
+      return renderUserProfilePage vm
+    | Success None -> 
+      return renderProfileNotFound
+    | Failure ex -> 
+      printfn "%A" ex
+      return page "server_error.liquid" "something went wrong"
+  } 
+    
 
-  let onFindUserFailure ex =
-    printfn "%A" ex
-    page "server_error.liquid" "something went wrong"
-
-
-  let renderUserProfile (getStreamClient : GetStream.Client) findUser username userMayBe ctx = async {
+  let renderUserProfile newUserProfileViewModel handleUserProfile username loggedInUser  ctx = async {
     match Username.TryCreate username with
     | Success validatedUsername -> 
-      let vm = 
-        {emptyUserProfileViewModel with
-           Username = validatedUsername.Value
-           ApiKey = getStreamClient.Config.ApiKey
-           AppId = getStreamClient.Config.AppId
-        }
-      match userMayBe with
-      | None -> 
-        let! webpart =
-            findUser validatedUsername
-            |> AR.either 
-                   (onFindUserSuccess getStreamClient vm) onFindUserFailure
-        return! webpart ctx
-      | Some (user : User) -> 
-        let vm = {vm with IsLoggedIn = true}
-        match user.Username = validatedUsername with
-        | true -> 
-          let vm = {vm with IsSelf = true}
-          return! onFindUserSuccess getStreamClient vm (Some user) ctx
-        | false ->
-          let! webpart =
-            findUser validatedUsername
-            |> AR.either (onFindUserSuccess getStreamClient vm) onFindUserFailure
-          return! webpart ctx
+      let! webpart = 
+        handleUserProfile validatedUsername loggedInUser
+        |> handleUserProfileAsyncResult newUserProfileViewModel loggedInUser
+      return! webpart ctx
     | Failure _ -> 
-      return! page "not_found.liquid" "invalid user name" ctx
+      return! renderProfileNotFound ctx
   }
 
 
   let webpart (getDataCtx : GetDataContext) getStreamClient = 
-    let findUser = Persistence.findUser getDataCtx
-    pathScan "/%s" 
-      (fun username -> 
-        mayRequiresAuth (renderUserProfile getStreamClient findUser username))
-    
+    let findUserProfile = findUserProfile (Persistence.findUser getDataCtx)
+    let handleUserProfile = handleUserProfile findUserProfile
+    let newUserProfileViewModel = newUserProfileViewModel getStreamClient
+    let renderUserProfile = renderUserProfile newUserProfileViewModel handleUserProfile
+    pathScan "/%s" (fun username -> mayRequiresAuth(renderUserProfile username))
