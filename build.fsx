71a9cc76a0524a6f34d2529ade42e699d7ff9a57
Demystify FP <demystifyfp@gmail.com>
2017-11-07 18:57:30 +0530

    adds log file

--- build.fsx
+++ build.fsx
@@ -90,6 +90,7 @@ Target "RevertDbConnStringChange" (fun _ ->
 )
 
 Target "CopyWebConfig" ( fun _ ->
+  FileHelper.CopyFile Kudu.deploymentTemp "iisnode.yml"
   FileHelper.CopyFile Kudu.deploymentTemp "web.config")
 
 Target "Deploy" Kudu.kuduSync
